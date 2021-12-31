5c8e68df546e16c7beffb81f483606a2c98eb564
ccw7259 <Charles.wu@yumchina.com>
2021-12-31 10:00:26 +0800

    Revert "sdsd"
    This reverts commit 346e07d60606aecd7992f9d6017a5703fd013894.

--- components/index.js
+++
@@ -1,5 +0,0 @@
-import dynamic from 'next/dynamic'
-
-export const DynamicLayout = dynamic(() => import('../../components/layout'))
-export const Test = dynamic(() => import('../../components/test'))
-export const Mdy = dynamic(() => import('../../components/m'))
